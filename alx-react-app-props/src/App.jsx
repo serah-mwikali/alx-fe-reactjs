ec3f2450c778629ed8ddd61c6148d4668ad372ac
pinky937 <mwikalis475@gmail.com>
2026-02-05 09:48:21 +0300

    Task 2: Refactor prop drilling to use Context API in UserProfile.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,16 +1,17 @@
-import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: 'Jane Doe', email: 'jane.doe@example.com' };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
     </UserContext.Provider>
   );
 }
 
 export default App;
+
 
 
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', width: '250px', margin: '10px auto' }}>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', width: '250px', margin: '20px auto' }}>
+      <h2>{userData.name}</h2>
+      <p>Email: {userData.email}</p>
     </div>
   );
 };
 
 export default UserProfile;
+
