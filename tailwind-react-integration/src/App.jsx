9ffda155a49efb7016383a164e7a822e781260b7
pinky937 <mwikalis475@gmail.com>
2026-02-19 00:06:50 +0300

    Styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,7 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind is Working 🎉
-      </h1>
-    </div>
-  )
+  return <UserProfile />;
 }
 
-export default App
+export default App;
