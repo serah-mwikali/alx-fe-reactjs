9e7329e1556116fd889b9920d783a7f6732d7d61
pinky937 <mwikalis475@gmail.com>
2026-02-18 23:56:59 +0300

    Initial commit - Tailwind CSS setup

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is Working 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
